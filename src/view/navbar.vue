36c574d66647509f9c7ca05af0e616fa4ed8fc57
黃俊龍 <a86527913@gmail.com~>
2025-05-05 11:00:31 +0800

    修正:navrbar顏色

--- src/view/navbar.vue
+++ src/view/navbar.vue
@@ -1,22 +1,23 @@
 <template>
 <div class="navbar">
-  <div>
+
     <div>
       <ul>
         <li><div class="link"><RouterLink to="/">Home</RouterLink></div></li>
       </ul>
     </div>
-    <ul>
-      
-      <li><div class="link"><RouterLink to="/01_props">01_props</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/02_emit">02_emit</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/03_event-bus">03_event-bus</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/04_V_model">04_V_model</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/05_useAttrs">05_useAttrs</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/06_ref">06_ref</RouterLink></div></li>
-      <li><div class="link"><RouterLink to="/07_provide_inject">07_provide_inject</RouterLink></div></li>
-    </ul>
-  </div>
+    <div>
+      <ul>
+        <li><div class="link"><RouterLink to="/01_props">01_props</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/02_emit">02_emit</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/03_event-bus">03_event-bus</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/04_V_model">04_V_model</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/05_useAttrs">05_useAttrs</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/06_ref">06_ref</RouterLink></div></li>
+        <li><div class="link"><RouterLink to="/07_provide_inject">07_provide_inject</RouterLink></div></li>
+      </ul>
+    </div>
+
   <div>
     <ul>
       <li><div class="link"><RouterLink to="/jslearn">/jslearn</RouterLink></div></li>
@@ -47,8 +48,6 @@ import { RouterLink } from 'vue-router';
   color: #fff;
   padding: 0 20px;
 }
-.link {
-  color: #fff;
-  text-decoration: none;
-}
+
+
 </style>
