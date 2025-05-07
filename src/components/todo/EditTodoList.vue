2409e28d181d975d38b76fda8328c253f1b568ea
黃俊龍 <a86527913@gmail.com~>
2025-05-07 17:20:27 +0800

    更新TodoList

--- src/components/todo/EditTodoList.vue
+++ src/components/todo/EditTodoList.vue
@@ -2,14 +2,17 @@
     <div>
         <h1>編輯代辦事項</h1>
     </div>
-    <input type="text" v-model="editContent">
+    <input type="text" v-model="editContent" ref="inputRef">
     <MyButton type="primary" @onClick="confirm">確認</MyButton>
     <MyButton type="cancel" @onClick="$emit('cancel')">取消</MyButton>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted, onBeforeUnmount } from 'vue'
+import { ref, watch, onMounted, onBeforeUnmount, nextTick } from 'vue'
 import MyButton from '@/view/ui/button.vue'
+
+const inputRef = ref<HTMLInputElement | null>(null)
+
 
 const props = defineProps<{
  modelValue: string
@@ -33,6 +36,11 @@ const confirm = () => {
 
 onMounted(() => {
     window.addEventListener('keydown', escHandler)
+
+    nextTick(() => {
+    inputRef.value?.focus()
+  })
+
 })
 
 onBeforeUnmount(() => {
