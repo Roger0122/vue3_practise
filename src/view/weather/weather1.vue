4aff2c7ca5c34890f3cde1706763e228ff1ee64d
黃俊龍 <a86527913@gmail.com~>
2025-05-29 16:52:04 +0800

    新增 搜尋資料

--- src/view/weather/weather1.vue
+++ src/view/weather/weather1.vue
@@ -1,48 +1,71 @@
 <template>
-<!-- 資料按鈕 -->
-<div><button @click="giveMeData">給我資料</button></div>
+  <!-- 搜尋列 -->
+  <div>
+    搜尋列
+    <input type="text" placeholder="輸入搜尋" v-model="seach">
+    <button class="px-1" @click="seachResult">搜尋</button>
+    <button @click="clearSearch">清除</button>
+  </div>
 
-<!-- 資料展示 -->
-<ul>
-  <li v-for="item in BackData">
-  {{ item.locationName }} {{ item.description }}
-  </li>
-</ul>
+  <!-- 資料按鈕 -->
+  <div>
+    <button @click="giveMeData">給我資料</button>
+  </div>
 
+  <!-- 資料展示 -->
+  <ul>
+    <li v-for="item in BackData" :key="item.locationName">
+      {{ item.locationName }}：{{ item.description }}
+    </li>
+  </ul>
 </template>
 
 <script setup>
-import axios  from 'axios';
-import { ref } from 'vue';
+import axios from 'axios'
+import { ref } from 'vue'
 
-const formData = ref([{}])
-
+const originalData = ref([])
 const BackData = ref([])
+const seach = ref('')
 
-
-const giveMeData =async () => {
-  await axios.get('http://localhost:3000/api/weather')
-  .then(res =>{
-    formData.value =res.data.records.location
-
-    BackData.value = formData.value.map(location => {
+// 取得資料並格式化
+const giveMeData = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/api/weather')
+    const parsed = res.data.records.location.map(location => {
       const locationName = location.locationName
-      
+
       const wxElement = location.weatherElement.find(el => el.elementName === 'Wx')
       const description = wxElement?.time?.[0]?.parameter?.parameterName ?? '無資料'
 
-       return {
+      return {
         locationName,
         description
-       }
+      }
     })
-  })
-  .catch(err => {
-    console.log(err)
-  })
- 
+
+    originalData.value = parsed
+    BackData.value = parsed
+  } catch (err) {
+    console.error('取得資料錯誤:', err)
+  }
 }
 
+
+const seachResult = () => {
+  BackData.value = originalData.value.filter(el =>
+    el.locationName.includes(seach.value)
+  )
+}
+
+
+const clearSearch = () => {
+  seach.value = ''
+  BackData.value = originalData.value
+}
+
+
+giveMeData()
 </script>
 
 <style scoped>
